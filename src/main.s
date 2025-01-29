f2ed32fd04ef37601ef58eaf8b170ce2f1e629a3
adam <adamaly93@gmail.com>
2025-01-29 22:59:46 +0000

    Enhance bootloader with magic number and update halt behavior

--- src/main.s
+++ src/main.s
@@ -1,7 +1,15 @@
 org 0
+.half
 
 main:
-    halt
+    wfi
 
-.halt:
-    jal .halt
+halt:
+    jal halt
+
+# 0x55AA is the magic number for the bootloader
+# BIOS Expects that the last 2 bytes of 512 byte sector is 0x55AA
+# So we pad the rest with 510 bytes
+.space 510 - (.-main)
+.byte 0x55
+.byte 0xAA
