1c0c87d549139b1d16952264ed201db324b2ef14
adam <adamaly93@gmail.com>
2025-02-01 22:47:13 +0000

    Initialize data segments and stack in bootloader

--- src/main.asm
+++ src/main.asm
@@ -2,6 +2,18 @@ org 0x7C00 ; BIOS loads the bootloader at 0x7C00
 bits 16    ; 16 bit mode for backward compatibility
 
 main:
+    
+    ; Set up data segments
+    mov ax, 0
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+
+    ; Set up stack
+    mov ss, ax 
+    mov sp, 0x7C00 ; Stack pointer is at beginning of bootloader (it grows downwards)
+    
     hlt
 
 .halt:
