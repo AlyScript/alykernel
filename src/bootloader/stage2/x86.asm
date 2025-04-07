6d3bfa29800656eb50c56e40469b12f334579a58
adam <adamaly93@gmail.com>
2025-04-07 08:29:59 +0100

    docs(x86.asm): implement x86_div64_32 function for 64-bit division

--- src/bootloader/stage2/x86.asm
+++ src/bootloader/stage2/x86.asm
@@ -2,6 +2,45 @@ bits 16
 
 section _TEXT class=CODE
 
+;
+; void _cdecl x86_div64_32(uint64_t dividend, uint32_t divisor, uint64_t* quotientOut, uint32_t* remainderOut);
+;
+global _x86_div64_32
+_x86_div64_32:
+
+    ; new stack frame
+    push bp        ; save old call frame
+    mov bp, sp     ; initialise new call frame
+
+    push bx
+
+    ; divide upper 32 bits
+    mov eax, [bp + 4]        ; eax <- upper 32 bits of divident
+    mov ecx, [bp + 12]       ; ecx <- divisor
+    xor edx, edx            ; clear edx
+    div ecx                 ; eax = eax / ecx, edx = eax % ecx
+
+    ; store upper 32 bits of quotient
+    mov ebx, [bp + 16]
+    mov [bx + 4], eax
+
+    ; divide lower 32 bits
+    mov eax, [bp + 4]        ; eax <- lower 32 bits of divident
+                             ; edx <- old remainder
+    div ecx
+
+    ; store lower 32 bits of quotient
+    mov [bx], eax
+    mov bx, [bp + 18]
+    mov [bx], edx           ; store remainder                 
+
+    pop bx
+
+    ; restore old stack frame
+    mov sp, bp
+    pop bp
+    ret
+
 global _x86_Video_WriteCharTeletype             ; '_' character is prefixed to names when using CDECL calling convention
 _x86_Video_WriteCharTeletype:
     
